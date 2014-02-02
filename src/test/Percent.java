ea3cd260ae569487fa95d7e680bf4cce9c9c64ff
joker23 <zhangksteven@gmail.com>
2014-02-01 23:50:55 -0500

    cleaned data projects and added percent parsing code...now I am ready to do some AI coding :D

---
+++ src/test/Percent.java
@@ -0,0 +1,42 @@
+import java.io.*;
+
+public class Percent{
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new FileReader(args[0]));
+		String str1;
+		String str2;
+
+		double avgwin = 0;
+		double avgloss = 0;
+		int one = 0;
+		int two = 0;
+
+		while((str1 = in.readLine()) != null){
+			str2 = in.readLine();
+
+			String[] sp1 = str1.split(" ");
+			String[] sp2 = str2.split(" ");
+			
+			int player = Integer.parseInt(sp1[1]);
+			int turns = Integer.parseInt(sp2[1]);
+			
+			if(player == 1){
+				one++;
+				avgwin += turns;
+			} else {
+				two++;
+				avgloss += turns;
+			}
+		}
+		double percent = ((double) one)/(one + two);
+		if(one > 0) avgwin /= (double) one;
+		if(two > 0) avgloss /= (double) two;
+
+		System.out.println("wins : " + one);
+		System.out.println("loss : " + two);
+		System.out.println("win percent : " +  percent);
+		System.out.println("average moves to win : " + avgwin);
+		System.out.println("average moves to loss : " + avgloss);
+	}
+}
